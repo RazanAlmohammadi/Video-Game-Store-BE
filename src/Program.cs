e4effb24e029ffef85b6f64af6fa2b669f9be5f1
RazanAlmohammadi <razanalmohammadi84@gmail.com>
2024-11-16 04:02:04 +0300

    Update

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,6 @@
+
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.FileProviders;
 var options = new WebApplicationOptions { WebRootPath = "wwwroot" };
 
 var builder = WebApplication.CreateBuilder(options);
@@ -148,6 +151,13 @@ app.UseCors(MyAllowSpecificOrigins);
 
 app.UseRouting();
 app.MapGet("/", () => "server is running");
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "wwwroot", "images")), // Correct path
+    RequestPath = "/images" // URL prefix to access images
+});
 
 //test if the database is conncted
 using (var scope = app.Services.CreateScope())
